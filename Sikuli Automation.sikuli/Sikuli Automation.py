4c1513520cb64aa6b00394d2ff11e1711c0f8adb
MondalAkash <35931882+MondalAkash@users.noreply.github.com>
2018-08-29 15:58:23 +0600

    Microsoft Office 2013 automated testing
    Automated Testing of Microsoft Office 2013 by using sikuliX Automation Tool

---
+++ Sikuli Automation.sikuli/Sikuli Automation.py
@@ -0,0 +1,411 @@
+
+rightClick("1523546812908.png")
+click("Screenshot_10.png")
+click("Screenshot_15.png")
+type("Automation")
+click("1523546946756.png")
+    
+if(exists("1523521029101.png")): 
+    doubleClick("1523514179500.png")
+if(exists("1523524852899.png")
+        ):
+    doubleClick("1523521223610.png")
+    
+    
+if(exists("1523516716029.png")):
+    print "Test Case 1 Passed"
+else:
+    print "Test case 1 failed"
+
+if(exists("1523517473214.png")):
+    click("1523514278996.png")
+if(exists("1523514309329.png")):
+    print "Test case 2 passed"
+else:
+    print "Test case 2 failed"
+click("1523516230447.png")
+if(exists("1523516262346.png")):
+    print "Test Case 3 Passed"
+else:
+    print "Test Case 3 Failed"
+click("1523516453877.png")
+if(exists("1523516505140.png")):
+   print "Test Case 4 Passed"
+else:
+    print "Test Case 4 Failed"
+click("1523516560077.png")
+if(exists("1523516633935.png")):
+    print "Test Case 5 Passed"
+else:
+    print "Test Case 5 Failed"
+click("1523516868984.png")
+
+if(exists("1523516882420.png")):
+    print "Test Case 6 Passed"
+else:
+    print "Test Case 6 Failed"
+click("1523516931891.png")
+if(exists("1523516976117.png")):
+    print "Test Case 7 Passed"
+else:
+    print "Test Case 7 Failed"
+click("1523517033795.png")
+if(exists("1523517056296.png")):
+    print "Test Case 8 Passed"
+else:
+    print "Test Case 8 Failed"
+click("1523517119409.png")
+if(exists("1523517141315.png")):
+    print "Test Case 9 Passed"
+else:
+    print "Test Case 9 Failed"
+click("1523517249032.png")
+if(exists("1523517805662.png")):
+    click("1523517341995.png")
+if(exists("1523517393379.png")):
+    print "Test Case 10 Passed"
+    click("1523518220901.png")
+else:
+    print "Test Case 10 Failed"
+if(exists("1523521029101.png")): 
+    doubleClick("1523514179500.png")
+if(exists("1523521788916.png")):
+    doubleClick("1523521807627.png") 
+click("1523516230447.png")
+click("1523518663780.png")
+if(exists("1523518709043.png")):
+    print"Test Case 11 Passed"
+else:
+    print"Test Case 11 Failed"
+click("1523518812374.png")
+if(exists("1523518867653.png")):
+    print "Test Case 12 Passed"
+    click("1523518947892.png")
+else:
+    print "Test Case 12 Failed"
+click("1523518975540.png")
+if(exists("1523519019819.png")):
+    print"Test Case 13 Passed"
+else:
+    print "Test Case 13 Failed"
+
+click("1523519039127.png")
+if(exists("1523519087570.png")):
+    print "Test Case 14 Passed"
+else:
+    print "Test Case 14 Failed"
+click("1523519210446.png")
+wait("1523519300329.png", 20)
+
+if(exists("1523519317125.png")):
+    print "Test Case 15 Passed"
+else:
+    print "Test Case 15 Failed"
+click("1523519392236.png")
+if(exists("1523519451020.png")):
+    print "Test Case 16 Passed"
+else:
+    print "Test Case 16 Failed"
+click("1523519527428.png")
+if(exists("1523519589622.png")):
+    print "Test Case 17 Passed"
+else:
+    print "Test Case 17 Failed"
+click("1523519629414.png")
+if(exists("1523519680377.png")):
+    print "Test Case 18 Passed"
+else:
+    print "Test Case 18 Failed"
+click("1523519702493.png")
+if(exists("1523519935988.png")):
+    print "Test Case 19 Passed"
+else:
+    print "Test Case 19 Failed"
+click("1523519963619.png")
+if(exists("1523520015953.png")):
+    print "Test Case 20 Passed"
+else:
+    print "Test Case 20 Failed"
+
+click("1523524054520.png")
+type("This is a Title")
+if(exists("1523524151288.png")):
+    print "Test Case 21 Passed"
+else:
+    print "Test Case 21 Failed"
+doubleClick("1523524345057.png")
+click("1523524497746.png")
+
+click("1523524374706.png")
+type("40")
+click("1523524590342.png")
+if(exists("1523524628933.png")):
+    print "Test Case 22 Passed"
+else:
+    print "Test Case 22 Failed"
+click("1523524960016.png")
+type("This is a Subtitle")
+doubleClick("1523525036542.png")
+click("1523525046623.png")
+click("1523525065072.png")
+type("45")
+click("1523525120792.png")
+if(exists("1523525169617.png")):
+    print "Test Case 23 Passed"
+else:
+    print "Test Case 23 Failed"
+doubleClick("1523525646162.png")
+click("1523525660917.png")
+click("1523525679449.png")
+if(exists("1523525706676.png")):
+    print "Test Case 24 Passed"
+else:
+    print "Test Case 24 Failed"
+doubleClick("1523525744000.png")
+click("1523525775728.png")
+click("1523525792798.png")
+if(exists("1523525881127.png")):
+    print "Test Case 25 Passed"
+else:
+    print "Test Case 25 Failed"
+doubleClick("1523525947231.png")
+click("1523525957419.png")
+click("1523525972697.png")
+if(exists("1523526005351.png")):
+    print "Tese Case 26 Passed"
+else:
+    print "Test Case 26 Failed"
+doubleClick("1523526083896.png")
+click("1523526095715.png")
+click("1523526124443.png")
+if(exists("1523526136253.png")):
+    print "Test Case 27 Passed"
+else:
+    print "Test Case 27 Failed"
+doubleClick("1523526226536.png")
+click("1523526233924.png")
+click("1523526271589.png")
+if(exists("1523526305816.png")):
+    print "Test Case 28 Passed"
+else:
+    print "Test Case 28 Failed"
+click("1523528842446.png")
+if(exists("1523528372715.png")):
+    print "Test Case 29 Passed"
+else:
+    print "Test Case 29 Failed"
+click("1523528582976.png")
+type("2nd silde title")
+click("1523528658032.png")
+if(exists("1523528716224.png")):
+    print "Test Case 30 Passed"
+else:
+    print "Test Case 30 Failed"
+click("1523530418781.png")
+click("1523530435866.png")
+click("1523530465267.png")
+click("Screenshot_7.png")
+click("1523530540604.png")
+if(exists("1523530624221.png")):
+    print "Tese Case 31 Passed"
+else:
+    print "Test Case 31 Failed"
+click("1523530840523.png")
+click("1523531772026.png")
+if(exists("1523530893915.png")):
+    print "Test Case 32 Passed"
+else:
+    print "Test Case 32 Failed"
+click("1523531902338.png")
+click("1523530943944.png")
+click("1523530993386.png")
+click("1523531017581.png")
+click("1523532021612.png")
+
+doubleClick("1524072147713.png")
+click("1524072449216.png")
+click("1523531213134.png")
+if(exists("1523532192881.png")):
+    print "Test Case 33 Passed"
+else:
+    print "Test Case 33 Failed"
+doubleClick("1523532226790.png")
+click("1523532276129.png")
+if(exists("1523532342064.png")):
+    print "Test Case 34 Passed"
+else:
+    print "Test Case 34 Failed"
+click("1523532581883.png")
+click("1523532592158.png")
+if(exists("1523532616532.png")):
+    print "Test Case 35 Passed"
+else:
+    print "Test Case 35 Failed"
+click("1523533310144.png")
+click("1523532865826.png")
+click("1523532888660.png")
+type("This is a textbox")
+click("1523532937875.png")
+if(exists("1523533453719.png")):
+    print "Test Case 36 Passed"
+else:
+    print "Test Case 36 Failed"
+
+click("1523533015648.png")
+click("1523530943944.png")
+click("1523533072841.png")
+click("Screenshot_8.png")
+if(exists("1523533171381.png")):
+    print "Test Case 37 Passed"
+else:
+    print "Test Case 37 Failed"
+click("1523533515996.png")
+click("1523533523194.png")
+click("1523533536057.png")
+click("1523533546068.png")
+if(exists("1523533602002.png")):
+    print "Test Case 38 Passed"
+else:
+    print "Test Case 38 Failed"
+click("1523533665777.png")
+click("1523534398977.png")
+if(exists("1523533713079.png")):
+    print "Test Case 39 Passed"
+else:
+    print "Test Case 39 Failed"
+rightClick("1523534245390.png")
+click("Screenshot_9.png")
+if(exists("1523533835991.png")):
+    print "Test Case 40 Passed"
+else:
+    print "Test Case 40 Failed"
+
+click("1523554821086.png")
+if(exists("1523555331221.png")):
+    print "Test Case 41 Passed"
+else:
+    print "Test Case 41 Failed"
+click("1523530943944.png")
+click("1523555507283.png")
+type("Smart Art Testing")
+click("1523556168069.png")
+click("1523556186061.png")
+click("1523630276986.png")
+
+click("1523630298203.png")
+click("1523630310578.png")
+
+
+if(exists("1523630329337.png")):
+    print "Test Case 42 Passed"
+else:
+    print "Test case 42 Failed"
+click("1523630739399.png")
+click("1523630749208.png")
+click("1523630762102.png")
+type("Quick Style Testing")
+click("1523630935118.png")
+click("1523630945504.png")
+click("Screenshot_18.png")
+if(exists("1523631079243.png")):
+    print "TC 43 Passed"
+else:
+    print "TC 43 Failed"
+click("1523631315702.png")
+click("1523631543631.png")
+if(exists("Screenshot_19-1.png")):
+    print "TC 44 Passed"
+else:
+    print "TC 44 Failed"
+click("1523631737552.png")
+click("1523631757851.png")
+click("1523631775834.png")
+if(exists("1523632311249.png")):
+    print "TC 45 Passed"
+else:
+    print "TC 45 Failed"
+click("1523632333629.png")
+if(exists("1523632412974.png")):
+    print "TC 46 Passed"
+else:
+    print "TC 46 Failed"
+click("1523632436167.png")
+if(exists("1523632478117.png")):
+    print"TC 47 Passed"
+else:
+    print "TC 47 Failed"
+click("1523632496591.png")
+if(exists("1523632543268.png")):
+    print "TC 48 Passed"
+else:
+    print "TC 48 Failed"
+
+click("1523632559516.png")
+if(exists("1523632606034.png")):
+    print "TC 49 Passed"
+else:
+    print"TC 49 Failed"
+click("1523632626321.png")
+if(exists("1523632672152.png")):
+    print "TC 50 Passed"
+else:
+    print "TC 50 Failed"
+click("1523632715129.png")
+if(exists("1523632758331.png")):
+    print "TC 51 Passed"
+else:
+    print "TC 51 Failed"
+click("1523632769137.png")
+if(exists("1523632821141.png")):
+    print "TC 52 Passed"
+else:
+    print "TC 52 Failed"
+click("1523632843583.png")
+if(exists("1523632870975.png")):
+    print "TC 53 Passed"
+else:
+    print "TC 53 Failed"
+click("1523633640396.png")
+click("1523633654362.png")
+if(exists("1523633694760.png")):
+    print "TC 54 Passed"
+else:
+    print "TC 54 Failed"
+click("1523633707275.png")
+if(exists("1523633753026.png")):
+    print "TC 55 Passed"
+else:
+    print "TC 55 Failed"
+click("1523633854534.png")
+if(exists("1523633892735.png")):
+    print "TC 56 Passed"
+else:
+    print "TC 56 Failed"
+click("1523633919604.png")
+if(exists("1523634003423.png")):
+    print "TC 57 Passed"
+else:
+    print "TC 57 Failed"
+click("1523634029433.png")
+click("1523634053280.png")
+if(exists("1523634276620.png")):
+    print "TC 58 Passed"
+else:
+    print "TC 58 Failed "
+click("1523634299498.png")
+click("1523634299498.png")
+click("1523634299498.png")
+if(exists("1523634340162.png")):
+    print "TC 59 Passed"
+else:
+    print "TC 59 Failed"
+click("1523634362146.png")
+if(exists("Screenshot_20.png")):
+    print "TC 60 Passed"
+else:
+    print "TC 60 Failed"
+
+
+
+
+
